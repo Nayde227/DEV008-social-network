eececa6a24f580d9c7a5fff236283af1f994e57e
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-07-04 16:09:12 -0400

    intento maskify

--- src/COMPONENTS/home.js
+++ src/COMPONENTS/home.js
@@ -20,11 +20,37 @@ export const home = (onNavigate) => {
   inputEmail.classList.add('inputHome')
   const inputPassword = document.createElement('input');
   inputPassword.classList.add('inputHome')
+  const getIntoPassword =inputPassword.value;
+  const masked = maskify(getIntoPassword);
   const email = document.createTextNode('E-mail');
   const password = document.createTextNode('Password');
   const logo = document.createElement('img');
   logo.src = '../logo.png';
   logo.classList.add('logo');
+
+  buttonLogin.addEventListener('click', () => {
+    if(!inputEmail.value || !inputPassword.value){
+      alert('Complete all fields correctly')
+    }else{loginUser(inputEmail.value, inputPassword.value)
+    loginUser(inputEmail.value, inputPassword.value)
+    .then(onNavigate('/login'))
+    .catch((e) => {
+      const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+      console.log(e)});
+}});
+function maskify(getIntoPassword){
+  const maskifygetIntoPassword = getIntoPassword.slice(0).replace(/ ./g,'#');
+  return maskifygetIntoPassword;
+}
+
+buttonRegister.addEventListener('click', () => onNavigate('/register'));
+buttonForgot.addEventListener('click', () => onNavigate('/forgot'));
+
+buttonLogin
+
 
   homeDiv.appendChild(logo);
   homeDiv.appendChild(email);
@@ -35,11 +61,5 @@ export const home = (onNavigate) => {
   homeDiv.appendChild(buttonRegister);
   homeDiv.appendChild(buttonForgot);
 
-  buttonLogin.addEventListener('click', () => {loginUser(inputEmail.value, inputPassword.value).then(onNavigate('/login')).catch((e) => {console.log(e)});
-  });
-  buttonRegister.addEventListener('click', () => onNavigate('/register'));
-  buttonForgot.addEventListener('click', () => onNavigate('/forgot'));
-
-  buttonLogin
   return homeDiv;
 };
